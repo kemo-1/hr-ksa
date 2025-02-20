56119660f220ce3c1f41b162322237982f93d485
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2025-02-21 00:40:42 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian'
 
 // https://astro.build/config
 export default defineConfig({
+
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			plugins: [
+				// Generate the Obsidian vault pages.
+				starlightObsidian({
+					output: 'docs',
+					sidebar: {
+						collapsed: false,
+						label: 'الفهرس'
+					},
+					vault: './vault',
+				}),
+			],
+			defaultLocale: 'root', // optional
+			locales: {
+				root: {
+					label: 'Arabic',
+					dir: 'rtl',
+					lang: 'ar', // lang is required for root locales
+				},
+			},
+			title: 'HR KSA',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				obsidianSidebarGroup
 			],
 		}),
 	],
